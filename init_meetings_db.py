3dab6492cd2d30c5c1517f0af2cc7abef6d03163
Delvin Dsouza <61946291+dxdelvin@users.noreply.github.com>
2026-04-08 22:41:17 +0200

    Delete init_meetings_db.py

--- init_meetings_db.py
+++
@@ -1,8 +0,0 @@
-import sys
-from app.core.database import engine
-from app.models.user import Base as ModelBase
-from app.models.meeting import MeetingFolder, MeetingNote, WhiteboardState
-
-print("Creating meeting tables if they don't exist...")
-ModelBase.metadata.create_all(bind=engine)
-print("Done!")
